48029099274399d84a3d035dc5754414100a1864
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-06-13 15:50:23 +1200

    Improve ASCII art algorithm
    Character shapes are now considered in the algorithm. The character
    is divided into sections and the intensity of the top, bottom,
    left, right and middle is calculated. The character with the
    closest shape to a 3x3 block of pixels is assigned to that area.

--- src/main.rs
+++ src/main.rs
@@ -38,27 +38,59 @@ fn run(options: Options) -> anyhow::Result<()> {
     println!("{} {}", new_width, new_height);
 
     println!("Resizing image");
-    let new_image = image::imageops::resize(&image, new_width, new_height, FilterType::Triangle);
-
+    let new_image = image::imageops::resize(&image, new_width * 3, new_height * 3, FilterType::Triangle);
+    let grayscale = image::imageops::grayscale(&new_image);
+    
     // TODO
     let path = std::path::Path::new(&options.font_filename);
     let data = std::fs::read(path).context("Failed to open the font file!")?;
     let font: Font = Font::try_from_vec(data).ok_or(anyhow::anyhow!("Failed to load font"))?;
 
-    let intensities = char_intensities("#@%xoe-/;:.,".chars(), font)?;
-    //println!("{:?}", intensities);
-    asciify(new_width, new_height, new_image, intensities);
+    let intensities = char_intensities((32u8..127u8).map(|x| x as char), font)?;
+    asciify(new_width, new_height, grayscale, intensities);
     Ok(())
 }
 
-type CharIntensities = Vec<(char, f32)>;
+/// Intensity divided as follows
+///
+/// lt t rt
+/// l  m  r
+/// lb b rt
+#[derive(Clone, Debug, Default)]
+struct Intensity {
+    left: f32,
+    right: f32,
+    top: f32,
+    bottom: f32,
+    middle: f32
+}
+impl Intensity {
+    fn distance(&self, other: &Intensity) -> f32 {
+        (self.left - other.left).abs()
+            + (self.right - other.right).abs()
+            + (self.top - other.top).abs()
+            + (self.bottom - other.bottom).abs() * 3.
+            + (self.middle - other.middle).abs()
+    }
+    fn apply<T: Fn(f32) -> f32>(&mut self, func: T) {
+        self.left = func(self.left);
+        self.right = func(self.right);
+        self.top = func(self.top);
+        self.bottom = func(self.bottom);
+        self.middle = func(self.middle);
+    }
+}
+type CharIntensities = Vec<(char, Intensity)>;
 
 fn char_intensities(chars: impl IntoIterator<Item = char>, font: Font) -> anyhow::Result<CharIntensities> {
-    let height: u32 = 24; // TODO fix
+    let height: u32 = 36; // TODO fix
+
+    // x = y means uniform scale
     let scale = Scale { x: height as f32, y: height as f32 };
 
     let mut maximum: f32 = 0.;
     let mut intensities: CharIntensities = Vec::new();
+    
     for character in chars {
         let glyph = font.glyph(character);
         let positioned = glyph
@@ -70,62 +102,115 @@ fn char_intensities(chars: impl IntoIterator<Item = char>, font: Font) -> anyhow
             + positioned.unpositioned().h_metrics().advance_width)
             .round() as u32;
 
-        let mut coverage: f32 = 0.;
-        positioned.draw(|_x, _y, v| {
-            //let v = (2. * (v - 0.5) + 0.5).min(0.).max(1.);
-            if v > 0.5 {
-                coverage += 1.;
-            }
-        });
 
-        let intensity = coverage / (width * height) as f32;
-        intensities.push((character, intensity));
-        maximum = maximum.max(intensity);
-    }
-    for pair in intensities.iter_mut() {
-        pair.1 /= maximum;
-        // TODO how to handle this case?
-        if pair.1 == std::f32::NAN {
-            anyhow::bail!("A character has an intensity of NAN!");
+        let side_width = width * 1 / 3;
+        let top_height = height * 1 / 3;
+
+        if let Some(bounding_box) = positioned.pixel_bounding_box() {
+            let mut intensity = Intensity::default();
+        
+            positioned.draw(|x, y, v| {
+                //let v = (2. * (v - 0.5) + 0.5).min(0.).max(1.);
+                let x = x as i32 + bounding_box.min.x;
+                let y = y as i32 + bounding_box.min.y;
+                let mut on_side = false;
+                if x < side_width as i32 {
+                    intensity.left += v;
+                    on_side = true;
+                }
+                if x >= (width - side_width) as i32 {
+                    intensity.right += v;
+                    on_side = true;
+                }
+                if y < top_height as i32 {
+                    intensity.top += v;
+                    on_side = true;
+                }
+                if y >= (height - top_height) as i32 {
+                    intensity.bottom += v;
+                    on_side = true;
+                }
+                if !on_side {
+                    intensity.middle += v;
+                }
+            });
+
+            intensity.bottom /= (top_height * width) as f32;
+            intensity.top /= (top_height * width) as f32;
+            intensity.left /= (side_width * height) as f32;
+            intensity.right /= (side_width * height) as f32;
+            intensity.middle /= ((width - side_width * 2) * (height - top_height * 2)) as f32;
+
+            maximum = maximum.max(intensity.bottom)
+                .max(intensity.top)
+                .max(intensity.left)
+                .max(intensity.right)
+                .max(intensity.middle);
+        
+            intensities.push((character, intensity));
         }
+    }
+    // normalise
+    for pair in intensities.iter_mut() {
+        pair.1.bottom /= maximum;
+        pair.1.top /= maximum;
+        pair.1.left /= maximum;
+        pair.1.right /= maximum;
+        pair.1.middle /= maximum;
+        
+        // pixels with more intensity are darker -- 0. = darkest, 1. = lightest.
+        pair.1.apply(|x| 1. - x);
     }
     // "a string".chars() does not have a space
-    intensities.push((' ', 0.));
-    intensities.sort_by(|a, b| OrderedFloat(a.1).cmp(&OrderedFloat(b.1)));
+    intensities.push((' ', Intensity {
+        bottom: 1.,
+        top: 1.,
+        left: 1.,
+        right: 1.,
+        middle: 1.
+    }));
     Ok(intensities)
 }
-fn intensity(pixel: &image::Rgba<u8>) -> f32 {
-    pixel[0] as f32 * 0.3 + pixel[1] as f32 * 0.59 + pixel[2] as f32 * 0.11
+
+fn avg_intensity(image: &image::GrayImage, x1: u32, y1: u32, x2: u32, y2: u32) -> f32 {
+    let mut s: u32 = 0;
+    for i in x1..x2 + 1 {
+        for j in y1..y2 + 1 {
+            s += image.get_pixel(i, j).0[0] as u32;
+        }
+    }
+    s as f32 / ((x2 + 1 - x1) * (y2 + 1 - y1)) as f32 / 256.
 }
-fn asciify(new_width: u32, new_height: u32, new_image: image::RgbaImage, char_intensities: CharIntensities) {
-    let mut maximum: f32 = 0.;
-    let mut minimum: f32 = std::f32::MAX;
+
+fn asciify(new_width: u32, new_height: u32, new_image: image::GrayImage, char_intensities: CharIntensities) {
+    //let mut maximum: f32 = 0.;
+    //let mut minimum: f32 = std::f32::MAX;
     
-    let mut intensities: Vec<Vec<f32>> = vec![vec![0.; new_height as usize]; new_width as usize];
+    let mut intensities: Vec<Intensity> = vec![Intensity::default(); (new_height * new_width) as usize];
     for j in 0..new_height {
         for i in 0..new_width {
-            let pixel = new_image.get_pixel(i, j);
-            let avg = intensity(pixel);
-             
-            intensities[i as usize][j as usize] = avg as f32;
-
-            if maximum < avg {
-                maximum = avg;
-            }
-            if minimum > avg {
-                minimum = avg;
-            }
+            let int = &mut intensities[(i + j * new_width) as usize];
+            // send help
+            int.left = avg_intensity(&new_image, 3 * i, 3 * j, 3 * i, 3 * j + 2);
+            int.right = avg_intensity(&new_image, 3 * i + 2, 3 * j, 3 * i + 2, 3 * j + 2);
+            int.top = avg_intensity(&new_image, 3 * i, 3 * j, 3 * i + 2, 3 * j);
+            int.bottom = avg_intensity(&new_image, 3 * i, 3 * j + 2, 3 * i + 2, 3 * j + 2);
+            int.middle = avg_intensity(&new_image, 3 * i + 1, 3 * j + 1, 3 * i + 1, 3 * j + 1);
         }
     }
     for j in 0..new_height {
         for i in 0..new_width {
-            let normalised = 1. - (intensities[i as usize][j as usize] - minimum) / (maximum - minimum);
-            let mut a = 2. * (normalised.powf(1.7) - 0.5) + 0.5;
-            a = a.max(0.).min(1.0);
+            let a = &mut intensities[(i + j * new_width) as usize];
+            //let normalised = 1. - (intensities[i as usize][j as usize] - minimum) / (maximum - minimum);
+            a.apply(|x: f32| {
+                let f: f32 = 1.12 * x.powf(0.8) + 0.06;
+                f.max(0.0).min(1.0)
+            });
+            //a = a.max(0.).min(1.0);
             let next_char = char_intensities
                 .iter()
-                .min_by_key(|(_, i)| OrderedFloat((i - a).abs()))
-                .unwrap_or(&(' ', 0.)).0; // todo
+                .min_by_key(|(_, x)| OrderedFloat(a.distance(&x)))
+                .unwrap_or(&(' ', Intensity::default())).0; // todo
             print!("{}", next_char);
         }
         println!();
